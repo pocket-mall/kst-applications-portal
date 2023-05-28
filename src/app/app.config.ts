a8f453a9a69dccb80dab427431679ab925235641
Chen, ZhongYi <zhongyi96chen@gmail.com>
2023-05-28 12:03:15 +0200

    chore: add angular material cdk

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { AuthenticationService } from './authentication/services/authentication.service';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), AuthenticationService]
+	providers: [provideRouter(routes), AuthenticationService, provideAnimations()]
 };
